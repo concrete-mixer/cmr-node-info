8b6302c2ad46c38c6a4f271739946b757ffc0850
Stuart McDonald <stuart@venturus.io>
2017-04-15 12:46:41 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Concrète Mixer web sockets track feed
+
+This repo contains a node server which receives messages from Concrète Mixer radio app and relays data about tracks being played to web clients on [https://concrete-mixer.github.io](https://concrete-mixer.github.io).
+
+If you're reading this I don't think this will be of any interest to you :D
---
+++ index.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var sockjs = require('sockjs');
+
+// https://github.com/MylesBorins/node-osc
+var osc = require('node-osc');
+
+
+var sockServer = sockjs.createServer({ sockjs_url: 'http://cdn.jsdelivr.net/sockjs/1.0.1/sockjs.min.js' });
+
+var oscServer = new osc.Server(2718, '0.0.0.0');
+
+sockServer.on('connection', function(conn) {
+  oscServer.on("message", function (msg, rinfo) {
+    const filePath = msg[1]
+    const stream = msg[2]
+    const matches = filePath.match(/_(.+)-(.+)\./)
+
+    if (matches && matches.length === 3) {
+      const streamData = {
+        recordist: matches[1],
+        file: matches[2],
+        stream: stream
+      }
+
+      conn.write(JSON.stringify(streamData))
+    }
+  });
+
+  conn.on('close', function() {});
+});
+
+var server = http.createServer();
+sockServer.installHandlers(server, {prefix:'/cm-data'});
+server.listen(9999, '0.0.0.0');
