13a26405cd035b6034eff0a3ab076da30a1569ca
Stuart McDonald <concretemixer.audio@gmail.com>
2017-04-16 22:21:14 +1200

    Changed regex for filename decoding and added soundcloud trackid (optional!)

--- index.js
+++ index.js
@@ -13,13 +13,14 @@ sockServer.on('connection', function(conn) {
   oscServer.on("message", function (msg, rinfo) {
     const filePath = msg[1]
     const stream = msg[2]
-    const matches = filePath.match(/_(.+)-(.+)\./)
+    const matches = filePath.match(/_(.+)_(.+)_(\d*)\./)
 
     if (matches && matches.length === 3) {
       const streamData = {
         recordist: matches[1],
         file: matches[2],
-        stream: stream
+        stream: stream,
+        soundcloudTrackId: matches[3] ? matches[3] : false
       }
 
       conn.write(JSON.stringify(streamData))
